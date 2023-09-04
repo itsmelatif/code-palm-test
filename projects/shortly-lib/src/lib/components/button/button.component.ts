9eeb64ff0df71e329428d74635c963359526efb4
itsmelatif <itsme.latif7@gmail.com>
2023-09-04 22:32:07 +0700

    shared-components: create button component

---
+++ projects/shortly-lib/src/lib/components/button/button.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ITypeButton } from '../models/components.model';
+
+@Component({
+  selector: 'lib-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+  @Input() label = '';
+  @Input() type: ITypeButton | undefined;
+
+  get classButton(){
+    return 'btn '+this.type;
+  }
+}
