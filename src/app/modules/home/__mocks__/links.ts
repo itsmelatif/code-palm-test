9417fd3877bb46b41e1ed0704f0963ebd8604f6c
itsmelatif <itsme.latif7@gmail.com>
2023-09-07 00:09:08 +0700

    feature/home: create mock list links

---
+++ src/app/modules/home/__mocks__/links.ts
@@ -0,0 +1,16 @@
+import { ICardLink } from "projects/shortly-lib/src/lib/models/components.model";
+
+export const mockLinkList: ICardLink[] = [
+  {
+    rawUrl: 'https://brimvoid.com',
+    shortUrl: 'https://re.link/siduia'
+  },
+  {
+    rawUrl: 'https://brimvoid.com',
+    shortUrl: 'https://re.link/siduia'
+  },
+  {
+    rawUrl: 'https://brimvoid.com',
+    shortUrl: 'https://re.link/siduia'
+  }
+]
