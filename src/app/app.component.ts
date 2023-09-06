d7a5df914d73fa4f112b4db221e77e40d18a9dc9
itsmelatif <itsme.latif7@gmail.com>
2023-09-06 21:59:10 +0700

    feature/card-component: dummy data card icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ICardIcon } from 'projects/shortly-lib/src/lib/models/components.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'code-palm-test-shortly';
+  dataCard: ICardIcon[] =  [
+    {
+      pathImage: 'assets/images/chart.svg',
+      title: 'Brand Recognition',
+      description: 'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.'
+    },
+    {
+      pathImage: 'assets/images/chart.svg',
+      title: 'Brand Recognition',
+      description: 'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.'
+    },
+    {
+      pathImage: 'assets/images/chart.svg',
+      title: 'Brand Recognition',
+      description: 'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.'
+    },
+    {
+      pathImage: 'assets/images/chart.svg',
+      title: 'Brand Recognition 4',
+      description: 'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.'
+    },
+    {
+      pathImage: 'assets/images/chart.svg',
+      title: 'Brand Recognition',
+      description: 'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.'
+    }
+  ]
 }
