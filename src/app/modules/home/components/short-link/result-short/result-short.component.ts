377ffa2e50bb1c36c8d28e9f9e70537df8501cf1
itsmelatif <itsme.latif7@gmail.com>
2023-09-07 00:10:01 +0700

    feature/home: call dummy data from service in result short component

--- src/app/modules/home/components/short-link/result-short/result-short.component.ts
+++ src/app/modules/home/components/short-link/result-short/result-short.component.ts
@@ -1,20 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ICardLink } from 'projects/shortly-lib/src/lib/models/components.model';
 import { Clipboard } from '@angular/cdk/clipboard';
-
+import { LinkService } from '../../../services/link.service';
+import { Subscription } from 'rxjs'
+import { finalize } from 'rxjs/operators';
 @Component({
   selector: 'app-result-short',
   templateUrl: './result-short.component.html',
   styleUrls: ['./result-short.component.scss']
 })
-export class ResultShortComponent {
-  listResults: ICardLink = {
-    rawUrl: 'https://brimvoid.com',
-    shortUrl: 'https://re.link/siduia'
+export class ResultShortComponent implements OnInit, OnDestroy {
+  isLoading = false;
+  listResults: ICardLink [] = [];
+  listSubscription: Subscription[] = [];
+
+  constructor(
+    private clipboard: Clipboard,
+    private _linkService: LinkService
+  ){
+
   }
 
-  constructor(private clipboard: Clipboard){
+  ngOnInit(): void {
+      this.onLoadResults();
+  }
 
+  ngOnDestroy(): void {
+      this.listSubscription.forEach(el => el.unsubscribe());
+  }
+
+  onLoadResults(){
+    this.isLoading = !this.isLoading;
+
+    const subsResults = this._linkService.listLinks$.pipe(
+      finalize(() => {
+        this.isLoading = this.isLoading;
+      })
+    ).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.listResults = res;
+      },
+      error: (e) => {
+        alert(e)
+      }
+    });
+
+    this.listSubscription.push(subsResults);
   }
 
   onCopy(data: string){
