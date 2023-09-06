8d57b78be6669867224332dc59c16aeb113f3735
itsmelatif <itsme.latif7@gmail.com>
2023-09-06 23:39:12 +0700

    feature/home: create model for prop card link component

--- projects/shortly-lib/src/lib/components/card-link/card-link.component.ts
+++ projects/shortly-lib/src/lib/components/card-link/card-link.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../card/card.component';
 import { ButtonComponent } from '../button/button.component';
+import { ICardLink } from '../../models/components.model';
 
 @Component({
   selector: 'lib-card-link',
@@ -10,6 +11,26 @@ import { ButtonComponent } from '../button/button.component';
   templateUrl: './card-link.component.html',
   styleUrls: ['./card-link.component.scss']
 })
-export class CardLinkComponent {
+export class CardLinkComponent implements OnInit{
 
+  @Input() data: ICardLink = {
+    rawUrl: 'string',
+    shortUrl: 'string'
+  }
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  renderComponent(): boolean{
+    return this.data.rawUrl.length > 0 && this.data.shortUrl.length > 0;
+  }
+
+  onCopy(){
+    alert('copy');
+  }
 }
--- projects/shortly-lib/src/lib/models/components.model.ts
+++ projects/shortly-lib/src/lib/models/components.model.ts
@@ -26,3 +26,8 @@ export interface ICardIcon {
   title: string;
   description: string;
 }
+
+export interface ICardLink {
+  rawUrl: string;
+  shortUrl: string;
+}
