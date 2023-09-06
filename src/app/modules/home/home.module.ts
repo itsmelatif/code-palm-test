6d755470bdf0ee3802a06e8dea09b5c4534ea1d6
itsmelatif <itsme.latif7@gmail.com>
2023-09-06 22:59:36 +0700

    feature/home: call short link component in home page component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,12 +5,18 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './components/components/home-page.component';
 import { CardStepComponent } from 'projects/shortly-lib/src/lib/components/card-step/card-step.component';
 import { StatisticListComponent } from './components/components/statistic-list/statistic-list.component';
+import { ShortLinkComponent } from './components/components/short-link/short-link.component';
+import { FormShortComponent } from './components/components/short-link/form-short/form-short.component';
+import { ResultShortComponent } from './components/components/short-link/result-short/result-short.component';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
     StatisticListComponent,
+    ShortLinkComponent,
+    FormShortComponent,
+    ResultShortComponent,
   ],
   imports: [
     CommonModule,
