1c8d57bbc5844964ae3fbfeb4cc801579dc82a25
itsmelatif <itsme.latif7@gmail.com>
2023-09-08 00:36:17 +0700

    bugfix/input-shorturl: implement controller value accessor in input text component

--- projects/shortly-lib/src/lib/components/input-text/input-text.component.ts
+++ projects/shortly-lib/src/lib/components/input-text/input-text.component.ts
@@ -1,19 +1,56 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, Optional, Self, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ITypeInputText, ITypeInputTextEnum } from '../../models/components.model';
+import { ControlValueAccessor, FormControl, ReactiveFormsModule, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'lib-input-text',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './input-text.component.html',
   styleUrls: ['./input-text.component.scss']
 })
-export class InputTextComponent {
+export class InputTextComponent implements ControlValueAccessor, OnInit, OnChanges {
   @Input() classText: ITypeInputText = 'primary';
   @Input() errorText = '';
   @Input() placeholder = '';
+
   @Output() changeValue: EventEmitter<string> = new EventEmitter<string>();
+
+
+  constructor(@Optional() @Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
+  }
+
+  get currentFormControl(): FormControl {
+    return this.ngControl.control as FormControl;
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.classText = changes['classText']?.currentValue;
+    this.errorText = changes['errorText']?.currentValue;
+  }
+
+  writeValue(value: string | string[]): void {
+
+  }
+
+  registerOnChange(onChange: (value: string | string[]) => void): void {
+
+  }
+
+  registerOnTouched(onTouched: () => void): void {
+
+  }
+
+  markAsTouched(): void {
+
+  }
+
 
   get classInput(): string{
     let label = 'inp inp-';
