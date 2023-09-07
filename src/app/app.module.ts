de1848318841eaed36c7ea402d931d9c0439d718
itsmelatif <itsme.latif7@gmail.com>
2023-09-08 00:36:52 +0700

    bugfix/input-shorturl: remove ununsed in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { MainComponent } from 'projects/shortly-lib/src/lib/layouts/main/main.component';
-import { CardComponent } from 'projects/shortly-lib/src/lib/components/card/card.component';
-import { ButtonComponent } from 'projects/shortly-lib/src/lib/components/button/button.component';
-import { CardStepComponent } from 'projects/shortly-lib/src/lib/components/card-step/card-step.component';
 import { CoreInterceptor } from './cores/interceptors/core.interceptor';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
