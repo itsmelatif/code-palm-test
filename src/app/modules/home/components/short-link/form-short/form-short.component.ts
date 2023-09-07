2fafadf4f42755560adb9ee1c445aca863293773
itsmelatif <saepul.latif@samudrainovasi.co.id>
2023-09-07 21:47:26 +0700

    feature/integrated-shortlink: add trigger on click button; integrated shorten url with form short component

--- src/app/modules/home/components/short-link/form-short/form-short.component.ts
+++ src/app/modules/home/components/short-link/form-short/form-short.component.ts
@@ -1,21 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ITypeInputText, ITypeInputTextEnum } from 'projects/shortly-lib/src/lib/models/components.model';
-
+import { ICardLink, ITypeInputText, ITypeInputTextEnum } from 'projects/shortly-lib/src/lib/models/components.model';
+import { LinkService } from '../../../services/link.service';
+import { ICommonResonse } from 'src/app/models/interfaces/common-response';
+import { IShortenResponse } from '../../../models/interfaces/shorten-response';
+import { Subscription } from 'rxjs'
 @Component({
   selector: 'app-form-short',
   templateUrl: './form-short.component.html',
   styleUrls: ['./form-short.component.scss']
 })
-export class FormShortComponent {
+export class FormShortComponent implements OnInit, OnDestroy {
   form: FormGroup;
+  subscription: Subscription[] = [];
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private linkService: LinkService
   ){
     this.form = this.fb.group({
       link: ['', [Validators.required]]
     })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.forEach(el => el.unsubscribe());
   }
 
   onChangeValue(value: string){
@@ -42,5 +55,25 @@ export class FormShortComponent {
         msg: 'This input field is required'
       }
       }
+    }
+
+    onShorten(){
+      const link = this.form.value.link;
+      const subsShort = this.linkService.shortenUrl(link).subscribe({
+        next: (res: ICommonResonse<IShortenResponse>) => {
+            const item: ICardLink = {
+              rawUrl: link,
+              shortUrl: res.result.full_short_link
+            }
+
+            this.linkService.updateLinks(item);
+        },
+        error: (e) => {
+          alert(e)
+        }
+      });
+
+      this.subscription.push(subsShort);
+
     }
 }
