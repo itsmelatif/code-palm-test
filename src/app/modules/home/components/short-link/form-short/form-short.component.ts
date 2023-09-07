f7c9675fdfc6133043d737fa681f8fa1094d97f6
itsmelatif <itsme.latif7@gmail.com>
2023-09-07 14:51:56 +0700

    feature/home: change ui form short component; add form builder in form short component

--- src/app/modules/home/components/short-link/form-short/form-short.component.ts
+++ src/app/modules/home/components/short-link/form-short/form-short.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ITypeInputText, ITypeInputTextEnum } from 'projects/shortly-lib/src/lib/models/components.model';
 
 @Component({
   selector: 'app-form-short',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-short.component.scss']
 })
 export class FormShortComponent {
+  form: FormGroup;
 
+  constructor(
+    private fb: FormBuilder
+  ){
+    this.form = this.fb.group({
+      link: ['', [Validators.required]]
+    })
+  }
+
+  onChangeValue(value: string){
+    this.form.patchValue({link: value});
+    console.log(this.form);
+  }
+
+  get errorForm(): {status: ITypeInputText, msg: string} {
+    if(!this.form.controls['link'].errors){
+      return {
+        status: ITypeInputTextEnum.success,
+        msg: ''
+      }
+    }
+
+    if(this.form.controls['link'].errors['required']){
+      return {
+        status: ITypeInputTextEnum.danger,
+        msg: 'This input field is required'
+      }
+    }else{
+      return {
+        status: ITypeInputTextEnum.danger,
+        msg: 'This input field is required'
+      }
+      }
+    }
 }
