0adab24917501a19af06cd583823f441dacce667
itsmelatif <itsme.latif7@gmail.com>
2023-09-08 20:33:36 +0700

    bugfix/svg-load: move load svg from component to service

--- projects/shortly-lib/src/lib/components/svg-image/svg-image.component.ts
+++ projects/shortly-lib/src/lib/components/svg-image/svg-image.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { ComponentsService } from '../../services/components.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'lib-svg-image',
@@ -10,24 +11,33 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
   templateUrl: './svg-image.component.html',
   styleUrls: ['./svg-image.component.scss']
 })
-export class SvgImageComponent {
-  svgContent!: SafeHtml;
+export class SvgImageComponent implements OnInit,  OnDestroy {
   @Input() urlPath: string = '';
+  svgContent!: SafeHtml;
+  subscription: Subscription[] = []
 
-  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
+  constructor(
+    private componentService: ComponentsService,
+    private sanitizer: DomSanitizer) {}
 
   ngOnInit() {
     this.loadSvgFile(this.urlPath);
   }
 
+  ngOnDestroy(): void {
+      this.subscription.forEach(el => el.unsubscribe());
+  }
+
   loadSvgFile(filePath: string) {
-    this.http.get(filePath, { responseType: 'text' }).subscribe(
-      (data) => {
+    const subsSvg = this.componentService.loadSvg(filePath).subscribe({
+      next: (data) => {
         this.svgContent = this.sanitizer.bypassSecurityTrustHtml(data);
       },
-      (error) => {
+      error: (error) => {
         console.error('Error loading SVG file:', error);
       }
-    );
+    })
+
+    this.subscription.push(subsSvg);
   }
 }
