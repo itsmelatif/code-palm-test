ed835ff928ecc7fc4451357f61cde36d45e7dead
itsmelatif <itsme.latif7@gmail.com>
2023-09-08 18:03:13 +0700

    layout/menu: add show and hide menu in mobile

--- projects/shortly-lib/src/lib/layouts/header/header.component.ts
+++ projects/shortly-lib/src/lib/layouts/header/header.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../components/button/button.component';
+import { ClickOutsideDirective } from '../../directives/click-outside.directive';
 
 @Component({
   selector: 'lib-header',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, ClickOutsideDirective],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  isVisible = false;
 
+  toggleVisibility() {
+    this.isVisible = !this.isVisible;
+  }
+
+  isClose(){
+    this.isVisible = !this.isVisible;
+  }
 }
